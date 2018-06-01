434127d4385938d3ddf765956ff24554cb628217
Patrick Gerritsen <p.gerritsen@wearetriple.com>
2018-06-01 11:03:03 +0200

    added a renderProp component and something to extract the import

--- index.js
+++ index.js
@@ -1,70 +1,99 @@
 import React from 'react';
+import hoistNonReactStatics from 'hoist-non-react-statics';
 
-const containerType = Symbol('StateContainer');
 
-export function Container() {
-    this.$$type = containerType;
-    this.listeners = [];
-    this.setState = function setState(updater, callback) {
-        const nextState = typeof updater === 'function'
-            ? updater(this.state)
-            : updater;
-        if (nextState == null) {
-            if (callback) callback();
-            return;
-        }
+const containerType = Symbol('withState');
 
-        this.state = Object.assign({}, this.state, nextState);
-
-        this.listeners.forEach(listener => listener());
-
-        if (callback) callback();
-    };
-    this.subscribe = function subscribe(fn) {
-        this.listeners.push(fn);
-    };
-    this.unsubscribe = function unsubscribe(fn) {
-        this.listeners = this.listeners.filter(f => f !== fn);
-    };
+function getDisplayName(reactComponent) {
+  return reactComponent.displayName || reactComponent.name || 'undefined';
 }
 
-export const withState = containerObject => (WrappedComponent) => {
-    const DUMMY_STATE = {};
-    const instances = Object.keys(containerObject).map((key) => {
-        const StateContainer = containerObject[key];
-        const container = StateContainer.$$type === containerType
-            ? StateContainer
-            : new StateContainer();
-        return {
-            name: key,
-            container,
-        };
+function initiateContainerInstances(containersObject) {
+  return Object.keys(containersObject).reduce((acc, key) => {
+    const StateContainer = containersObject[key];
+    const ContainerInstance = StateContainer.$$type === containerType
+      ? StateContainer
+      : new StateContainer()
+    return Object.assign(acc, {
+      [key]: ContainerInstance
+    })
+  }, {});
+}
+
+export function Container() {
+  let listeners = [];
+  this.$$type = containerType;
+  this.subscribe = function subscribe(fn) {
+    listeners.push(fn);
+  }
+  this.unsubscribe = function unsubscribe(fn) {
+    listeners = listeners.filter(f => f !== fn);
+  }
+  this.setState = function setState(updater, callback) {
+    // create the updated state
+    const updatedState = typeof updater === 'function'
+      ? updater(this.state)
+      : updater;
+
+    // if the updatedState is null or undefined, don't update the state
+    if (updatedState == null) {
+      if (callback) callback();
+      return;
+    }
+
+    // update the state by creating a new object and merging them all together
+    this.state = Object.assign({}, this.state, updatedState);
+
+    // call all the subscribers that there is a new state
+    listeners.forEach(listener => listener());
+
+    // and finally, call the callback
+    if (callback) callback();
+  }
+}
+
+export class WithState extends React.Component {
+  constructor(props) {
+    super(props);
+    this.EMPTY_OBJECT = {};
+    this.unmounted = false;
+    this.onUpdate = this.onUpdate.bind(this);
+    this.instances = initiateContainerInstances(props.containers);
+  }
+  componentDidMount() {
+    Object.keys(this.instances).forEach(key => {
+      this.instances[key].subscribe(this.onUpdate)
     });
+  }
+  componentWillUnmount() {
+    Object.keys(this.instances).forEach(key => {
+      this.instances[key].unsubscribe(this.onUpdate)
+    });
+  }
+  onUpdate() {
+    if (!this.unmounted) {
+      this.setState(this.EMPTY_OBJECT);
+    }
+  }
+  render() {
+    return this.props.children(this.instances);
+  }
+}
 
-    return class WithState extends React.Component {
-        unmounted = false;
+export const withState = containers => WrappedComponent => {
+  const wrappedComponentDisplayName = getDisplayName(WrappedComponent);
+  const displayName = `withState(${wrappedComponentDisplayName})`;
+  const ComponentWithStateContainer = props =>
+    React.createElement(
+      WithState,
+      { containers },
+      (containerInstances) => React.createElement(
+        WrappedComponent,
+        Object.assign({}, props, containerInstances),
+      ),
+    );
 
-        componentDidMount() {
-            instances.forEach(instance => instance.container.subscribe(this.onChange));
-        }
-        componentWillUnmount() {
-            this.unmounted = true;
-            instances.forEach(instance => instance.container.unsubscribe(this.onChange));
-        }
-        onChange = () => {
-            this.setState(DUMMY_STATE);
-        }
+    ComponentWithStateContainer.displayName = displayName;
 
-        render() {
-            const mapContainerState = instances.reduce((acc, { name, container }) => ({
-                ...acc,
-                [name]: container,
-            }), {});
-            const props = {
-                ...this.props,
-                ...mapContainerState,
-            };
-            return <WrappedComponent {...props} />;
-        }
-    };
-};
+    return hoistNonReactStatics(ComponentWithStateContainer, WrappedComponent);
+}
